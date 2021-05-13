c498762ea5e30febf9fb4d5bb77eeb2c4833c3dd
Michael Morgan <self@msmorgan.dev>
2021-05-13 13:13:32 -0400

    Sock merchant solution and test

---
+++ hackerrank/interviewprep/tests/__init__.py
@@ -0,0 +1,14 @@
+from textwrap import dedent
+
+
+def patch_io(module: object, input: str) -> list[str]:
+    input_iter = iter(dedent(input).strip().splitlines())
+    module.input = lambda *_: next(input_iter)
+    print_list = []
+
+    def patched_print(*args, sep=' '):
+        print_list.append(sep.join(*map(str, args)))
+
+    module.print = patched_print
+
+    return print_list
---
+++ hackerrank/interviewprep/tests/warmup/sock_merchant.py
@@ -0,0 +1,26 @@
+import unittest
+from textwrap import dedent
+
+from hackerrank.interviewprep.warmup import sock_merchant
+from hackerrank.interviewprep.tests import patch_io
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        output_lines = patch_io(
+            sock_merchant,
+            """
+            9
+            10 20 20 10 10 30 50 10 20
+            """,
+        )
+
+        sock_merchant.main()
+
+        expected_output = "3"
+        actual_output = '\n'.join(output_lines)
+
+        self.assertEqual(expected_output, actual_output)
+
+
+if __name__ == '__main__':
+    unittest.main()
