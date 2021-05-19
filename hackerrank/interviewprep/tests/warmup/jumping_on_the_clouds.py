bc58507e19c466f3eba4cd5b0f85553c6752c8b5
Michael Morgan <self@msmorgan.dev>
2021-05-19 00:43:19 -0400

    Jumping on the clouds v1

---
+++ hackerrank/interviewprep/tests/warmup/jumping_on_the_clouds.py
@@ -0,0 +1,13 @@
+import unittest
+
+from hackerrank.interviewprep.warmup import jumping_on_the_clouds
+from hackerrank.interviewprep.tests import patch_io
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ hackerrank/interviewprep/warmup/jumping_on_the_clouds.py
@@ -0,0 +1,20 @@
+def jumpingOnClouds(c):
+    i = 0
+    step = 0
+    while i < len(c)-1:
+        if i + 2 < len(c) and c[i + 2] == 0:
+            i += 2
+        else:
+            i += 1
+        step += 1
+    return step
+
+
+def main():
+    n = int(input())
+    c = list(map(int, input().strip().split()))
+    print(jumpingOnClouds(c))
+
+
+if __name__ == "__main__":
+    main()
